fd6929482cd69492d89ba57043b25966d63559f1
Andriy_Antonik <zadrotdd1995@gmail.com>
2021-11-29 11:02:53 +0200

    InitProject

---
+++ Antonyk_Andrii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Antonyk_Andrii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
